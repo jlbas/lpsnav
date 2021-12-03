4e8bb41681c3705135384467923dae1e970305aa
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2021-12-03 08:46:51 -0500

    Fix `__init__()` for inattentive class

--- legitable/policies/inattentive.py
+++ legitable/policies/inattentive.py
@@ -2,6 +2,6 @@ from policies.agent import Agent
 
 
 class Inattentive(Agent):
-    def __init__(self, config, env, id, policy, start, goal):
-        super().__init__(config, env, id, policy, start, goal)
+    def __init__(self, config, env, id, policy, start, goal=None, max_speed=None):
+        super().__init__(config, env, id, policy, start, goal=goal, max_speed=max_speed)
         self.color = "#868293"
