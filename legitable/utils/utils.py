65f684f4acd1282b66507c9d5a20ad904ec71b4d
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2022-02-08 15:19:09 -0500

    Remove `print_to_file()` since were using the `logging` module now

--- legitable/utils/utils.py
+++ legitable/utils/utils.py
@@ -1,13 +1,4 @@
 import argparse
-import sys
-
-
-def print_to_file(file, message):
-    original_stdout = sys.stdout
-    with open(file, "w") as outfile:
-        sys.stdout = outfile
-        print(message)
-        sys.stdout = original_stdout
 
 
 def get_args():
