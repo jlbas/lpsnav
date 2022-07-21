90c18a06e8cd705e9b1637507971fb3190b8ecfa
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2022-07-21 07:29:05 -0400

    Pass missing `default_rng()` argument to inattentive class

--- lpsnav/policies/inattentive.py
+++ lpsnav/policies/inattentive.py
@@ -2,5 +2,5 @@ from policies.agent import Agent
 
 
 class Inattentive(Agent):
-    def __init__(self, conf, id, policy, is_ego, max_speed, start, goal):
-        super().__init__(conf, id, policy, is_ego, max_speed, start, goal)
+    def __init__(self, conf, id, policy, is_ego, max_speed, start, goal, rng):
+        super().__init__(conf, id, policy, is_ego, max_speed, start, goal, rng)
