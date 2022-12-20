02d9aac13e73e7f001ccf0020372917ae99c8d34
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2022-12-20 09:27:34 -0500

    Rename `rvo` to `orca`

--- lpsnav/policies/orca.py
+++ lpsnav/policies/orca.py
@@ -4,7 +4,7 @@ from policies.agent import Agent
 from utils import helper
 
 
-class Rvo(Agent):
+class Orca(Agent):
     def __init__(self, conf, id, policy, is_ego, max_speed, start, goal, rng):
         super().__init__(conf, id, policy, is_ego, max_speed, start, goal, rng)
         self.neighbor_dist = conf["neighbor_dist"]
@@ -14,7 +14,7 @@ class Rvo(Agent):
 
     def post_init(self, dt, agents):
         super().post_init(dt, agents)
-        self.rvo_sim = rvo2.PyRVOSimulator(
+        self.orca_sim = rvo2.PyRVOSimulator(
             dt,
             self.neighbor_dist,
             self.max_neighbors,
@@ -23,21 +23,21 @@ class Rvo(Agent):
             self.expanded_radius,
             self.max_speed,
         )
-        self.rvo_sim.processObstacles()
-        self.rvo_agents = {self.id: self.rvo_sim.addAgent(tuple(self.pos))}
+        self.orca_sim.processObstacles()
+        self.orca_agents = {self.id: self.orca_sim.addAgent(tuple(self.pos))}
         for k, a in agents.items():
-            self.rvo_agents[k] = self.rvo_sim.addAgent(tuple(a.pos))
+            self.orca_agents[k] = self.orca_sim.addAgent(tuple(a.pos))
 
     def get_action(self, dt, agents):
-        self.rvo_sim.setAgentPosition(self.rvo_agents[self.id], tuple(self.pos))
-        self.rvo_sim.setAgentVelocity(self.rvo_agents[self.id], tuple(self.vel))
+        self.orca_sim.setAgentPosition(self.orca_agents[self.id], tuple(self.pos))
+        self.orca_sim.setAgentVelocity(self.orca_agents[self.id], tuple(self.vel))
         for k, a in agents.items():
-            self.rvo_sim.setAgentPosition(self.rvo_agents[k], tuple(a.pos))
-            self.rvo_sim.setAgentVelocity(self.rvo_agents[k], tuple(a.vel))
+            self.orca_sim.setAgentPosition(self.orca_agents[k], tuple(a.pos))
+            self.orca_sim.setAgentVelocity(self.orca_agents[k], tuple(a.vel))
         pref_vel = self.max_speed * helper.unit_vec(self.goal - self.pos)
-        self.rvo_sim.setAgentPrefVelocity(self.rvo_agents[self.id], tuple(pref_vel))
-        self.rvo_sim.doStep()
+        self.orca_sim.setAgentPrefVelocity(self.orca_agents[self.id], tuple(pref_vel))
+        self.orca_sim.doStep()
 
-        dpos = self.rvo_sim.getAgentPosition(self.rvo_agents[self.id]) - self.pos
+        dpos = self.orca_sim.getAgentPosition(self.orca_agents[self.id]) - self.pos
         self.des_speed = np.linalg.norm(dpos) / dt
         self.des_heading = helper.angle(dpos)
